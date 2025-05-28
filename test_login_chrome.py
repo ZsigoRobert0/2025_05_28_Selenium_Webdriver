f9726e309d1a50f8c0564e8fcbf026505a3cf716
Zsigo Robert <zsigo.robert.jozsef@diak.szbi-pg.hu>
2025-05-28 08:48:47 +0200

    üres sor

--- test_login_chrome.py
+++ test_login_chrome.py
@@ -18,6 +18,7 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
+
 try:
     # Oldal megnyitása
     driver.get("https://parking-garage-app.netlify.app/")
